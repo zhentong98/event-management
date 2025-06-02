a6dabc47954ae28b27f89ccb9acf01f49cb73e33
zhentong98 <zhentong98@hotmail.com>
2025-06-02 22:24:24 +0800

    storing data and validation

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $event_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Event|null $event
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee whereEventId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Attendee whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Attendee extends Model
 {
     use HasFactory;
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,9 +7,39 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $name
+ * @property string|null $description
+ * @property string $start_time
+ * @property string $end_time
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Attendee> $attendees
+ * @property-read int|null $attendees_count
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\EventFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereEndTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereStartTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
 
     public function user(): BelongsTo
     {
